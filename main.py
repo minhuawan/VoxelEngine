5ed7eca29218052579d2126b4f7e8da7ed93278d
Wan Min Hua <minhua.wan17@gmail.com>
2024-06-04 13:58:14 +0800

    Phase 1, create opengl window

---
+++ main.py
@@ -0,0 +1,55 @@
+import sys
+
+import moderngl as mgl
+import pygame as pg
+
+from setting import *
+
+
+class VoxelEngine:
+    def __init__(self):
+        pg.init()
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE)
+        pg.display.gl_set_attribute(pg.GL_DEPTH_SIZE, 24)
+
+        pg.display.set_mode(WIN_RES, flags=pg.OPENGL | pg.DOUBLEBUF)
+
+        self.ctx = mgl.create_context()
+        self.ctx.enable(flags=mgl.DEPTH_TEST | mgl.CULL_FACE | mgl.BLEND)
+        self.ctx.gc_mode = 'auto'
+
+        self.clock = pg.time.Clock()
+        self.delta_time = 0
+        self.time = 0
+
+        self.is_running = False
+
+    def update(self):
+        self.delta_time = self.clock.tick()
+        self.time = pg.time.get_ticks() * 0.001
+        pg.display.set_caption(f'Fps {self.clock.get_fps():.0f}')
+
+    def render(self):
+        self.ctx.clear(color=BG_COLOR)
+        pg.display.flip()
+
+    def handle_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                self.is_running = False
+
+    def run(self):
+        self.is_running = True
+        while self.is_running:
+            self.handle_events()
+            self.update()
+            self.render()
+        pg.quit()
+        sys.exit()
+
+
+if __name__ == '__main__':
+    app = VoxelEngine()
+    app.run()
