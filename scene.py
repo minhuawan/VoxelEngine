4228190a2f332326f3dc89a4433a72455ec7ed5b
minhua.wan <minhua.wan17@gmail.com>
2024-06-12 13:45:43 +0800

    Phase 4.5: create a white voxel

--- scene.py
+++ scene.py
@@ -1,16 +1,19 @@
 from meshs.quad_mesh import QuadMesh
 from meshs.triangle_mesh import TriangleMesh
+from world_objects.chunk import Chunk
 
 
 class Scene:
     def __init__(self, app):
         self.app = app
-        self.quad = QuadMesh(self.app)
-        self.triangle = TriangleMesh(self.app)
+        # self.quad = QuadMesh(self.app)
+        # self.triangle = TriangleMesh(self.app)
+        self.chunk = Chunk(self.app)
 
     def update(self):
         pass
 
     def render(self):
-        self.quad.render()
+        # self.quad.render()
         # self.triangle.render()
+        self.chunk.render()
--- settings.py
+++ settings.py
@@ -1,9 +1,16 @@
 import math
 
 import glm
+import numpy as np
 
 # resolution
 WIN_RES = glm.vec2(1600, 900)
+
+# chunk
+CHUNK_SIZE = 32
+H_CHUNK_SIZE = CHUNK_SIZE // 2
+CHUNK_AREA = CHUNK_SIZE * CHUNK_SIZE
+CHUNK_VOL = CHUNK_AREA * CHUNK_SIZE
 
 # camera @see docs/capture_settings_camera_1.png
 ASPECT_RATIO = WIN_RES.x / WIN_RES.y
@@ -17,7 +24,7 @@ PITCH_MAX = glm.radians(89)
 # player
 PLAYER_SPEED = 0.005
 PLAYER_ROT_SPEED = 0.003
-PLAYER_POS = glm.vec3(0, 0, 1)
+PLAYER_POS = glm.vec3(H_CHUNK_SIZE, CHUNK_SIZE, 1.5 * CHUNK_SIZE)
 MOUSE_SENSITIVITY = 0.002
 
 # colors
